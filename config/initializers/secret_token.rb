394978cfbad7c90bbfeddf2bd751c10141b867cf
Julien Fusco <pkoin.koin@gmail.com>
2013-02-09 12:21:23 +0100

    Add: rails (4.0.0.beta) new --edge

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ApiFakeIo::Application.config.secret_key_base = '9918c89e93a626f76cfe3c794a4a6014357158989b0f3a748031e028acb58389a3f6b17c7a688610564f299b3b447cd494a37282e6f3a12fe8bb503f357d2eaa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ApiFakeIo::Application.config.session_store :encrypted_cookie_store, key: '_api_fake_io_session'
